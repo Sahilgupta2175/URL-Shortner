fca3d4410fba3914d3514a8820459206a7706ec7
Sahil Gupta <guptasahil2175@gmail.com>
2025-10-27 21:21:46 +0530

    refactor(api): rename longUrl to originalUrl and enhance short URL response
    Rename the input URL variable to originalUrl for clarity and to avoid confusion with the generated short URL.
    Change short URL code length from 7 to 10 characters for better uniqueness.
    Update API response to include original URL, generated short code, and full short URL for improved usability.

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -4,29 +4,32 @@ import { nanoid } from "nanoid";
 
 const shortUrl = async (req, res) => {
     const { longUrl } = req.body;
-    console.log("Received long url from user: ", longUrl);
+    const originalUrl = longUrl;
+    console.log("Received long url from user: ", originalUrl);
     
     //* checking longUrl exist or not
-    if(!longUrl) {
+    if(!originalUrl) {
         return res.status(400).json({success: false, message: 'Please provide a URL.'});
     }
 
     //* checking longUrl is valid or not. eg. http://www.google.com is valid || www.google.com is not valid
-    if(!validUrl.isUri(longUrl)) {
+    if(!validUrl.isUri(originalUrl)) {
         return res.status(400).json({success: false, error: 'Invalid URL formate provided.'});
     }
 
     try {
         //* finding url is exist or not in db
-        let url = await URL.findOne({longUrl: longUrl});
+        let url = await URL.findOne({originalUrl: originalUrl});
         
         if(url) {
             return res.json({success: true, data: url});
         }
         
-        const urlCode = nanoid(7);
+        const generatedShortURL = nanoid(10);
 
-        res.status(200).json({success: true, message: 'URL is new. Code generate successfully.', data: {urlCode: urlCode}});
+        const longUrl = `${process.env.BASE_URL}/${generatedShortURL}`;
+
+        res.status(200).json({success: true, message: 'URL is new. Code generate successfully.', data: {originalUrl: originalUrl, generatedShortURL: generatedShortURL, longUrl: longUrl}});
     } catch (error) {
         console.log('Database error.', error.message);
         return res.status(500).json({success: false, error: 'Internal Server error.'});
