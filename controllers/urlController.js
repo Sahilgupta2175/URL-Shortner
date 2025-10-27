f389087ea0b20cc42145fa45fe025ab9bfb08cc0
Sahil Gupta <guptasahil2175@gmail.com>
2025-10-27 21:07:23 +0530

    feat(api): generate short URL code for new URLs
    Add nanoid import and logic to create a unique 10-character code if the long URL is not found in the database, enabling the URL shortening process.

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,26 +1,32 @@
 import validUrl from "valid-url";
 import URL from "../models/url.js";
+import { nanoid } from "nanoid";
 
 const shortUrl = async (req, res) => {
     const { longUrl } = req.body;
     console.log("Received long url from user: ", longUrl);
     
+    //* checking longUrl exist or not
     if(!longUrl) {
         return res.status(400).json({success: false, message: 'Please provide a URL.'});
     }
 
+    //* checking longUrl is valid or not. eg. http://www.google.com is valid || www.google.com is not valid
     if(!validUrl.isUri(longUrl)) {
         return res.status(400).json({success: false, error: 'Invalid URL formate provided.'});
     }
 
     try {
+        //* finding url is exist or not in db
         let url = await URL.findOne({longUrl: longUrl});
         
         if(url) {
             return res.json({success: true, data: url});
         }
         
-        res.status(200).json({success: true, message: 'URL is new and valid. Ready to be shortened.'});
+        const urlCode = nanoid(7);
+
+        res.status(200).json({success: true, message: 'URL is new. Code generate successfully.', data: {urlCode: urlCode}});
     } catch (error) {
         console.log('Database error.', error.message);
         return res.status(500).json({success: false, error: 'Internal Server error.'});
